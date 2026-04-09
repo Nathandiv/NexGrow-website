689eedc0c14f71e34bcbd75c157e1c40b6868807
Nathan Hlungwani <nathan.hlungwani123@gmail.com>
2026-04-09 19:33:35 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-// Import your components here
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: AppComponent,
     pathMatch: 'full'
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'services',
-    component: ServicesComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
   },
   {
     path: '**',
@@ -34,7 +17,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled' // nice UX
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled'
     })
   ],
   exports: [RouterModule]
